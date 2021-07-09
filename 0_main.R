7982601ecfb2e44a76dff8a4d45180594d171e44
dr-rosen-rosen <mrosen44@jhmi.edu>
2021-07-09 09:10:20 -0400

    sadf

---
+++ 0_main.R
@@ -0,0 +1,18 @@
+##########################################################################
+##########################################################################
+################# Main File for Patient MD Communication Analysis
+##########################################################################
+##########################################################################
+
+library(here)
+library(config)
+library(reticulate)
+library(lubridate)
+library(tidyverse)
+library(patchwork)
+debuggingState(on=FALSE)
+# start ve with: source python3/bin/activate in project folder
+Sys.setenv(R_CONFIG_ACTIVE = "default")
+config <- config::get()
+Sys.setenv(RETICULATE_PYTHON = config$py_version)
+#reticulate::source_python('1_funcs.py')
