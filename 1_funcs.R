bc866674df4aafea2ecf025d36df1aa692ccf385
salarkhaleghzadegan <skhaleg1@gmail.com>
2021-09-14 08:36:29 -0400

    updating the cleaning code for the transcripts

--- 1_funcs.R
+++ 1_funcs.R
@@ -11,9 +11,10 @@ library(tidyverse)
 library(stringr)
 
 all_filenames <- list.files(
-  path = "/Users/skhaleg1/OneDrive - Johns Hopkins/Patient Provider Communication Projects/Analysis/Patient_MD_Com", full.names = FALSE,
+  path = "/Users/skhaleg1/OneDrive - Johns Hopkins/Patient _Provider_Communication_Projects/Analysis/Maripohsa_transcripts", full.names = TRUE,
   pattern = ".docx", recursive = TRUE)
 
+#initial data cleaning function
 data_cleaning_transcript <- function(.x) {
   #Import raw data from MS Word file
   audio_doc <- docxtractr::read_docx(.x)
@@ -39,13 +40,48 @@ data_cleaning_transcript <- function(.x) {
   
 }
 
-
+#running 
 big_tibble_test <-all_filenames %>% purrr::map_dfr(~data_cleaning_transcript(.))
 
 
 #creating a variable that has utterance order for each transcript
-all_transcripts_final <-big_tibble %>% group_by(patient_id) %>% mutate(utterance_order = 1:n())
+all_transcripts_final <-big_tibble_test %>% group_by(patient_id) %>% mutate(utterance_order = 1:n())
 
 
+#cleaning to make timestamp and role variables
+all_transcripts_final <- all_transcripts_final %>%
+  filter(V1 != "") %>%
+  filter(V1 != "?") %>%
+  filter(V1 != "[silence]") %>%
+  filter(str_detect(V1, "")) %>%
+  mutate(timestamp = str_match(V1, "(\\d{2}:\\d{2})")[, 2]) %>%
+  mutate(role1 = str_match(V1, "((?i)doctor)")[, 2]) %>%
+  mutate(role2 = str_match(V1, "((?i)patient)")[, 2]) %>%
+  mutate(role3 = str_match(V1, "((?i)other)")[, 2]) %>%
+  mutate(role4 = str_match(V1, "((?i)S1)")[, 2]) %>%
+  mutate(role5 = str_match(V1, "((?i)S2)")[, 2]) %>%
+  mutate(role6 = str_match(V1, "((?i)S3)")[, 2]) %>%
+  mutate(role7 = str_match(V1, "((?i)S4)")[, 2]) %>%
+  mutate(role8 = str_match(V1, "((?i)S5)")[, 2]) %>%
+  mutate(role9 = str_match(V1, "((?i)S6)")[, 2]) %>%
+  mutate(role10 = str_match(V1, "((?i)S7)")[, 2]) %>%
+  mutate(role11 = str_match(V1, "((?i)S8)")[, 2]) %>%
+  mutate(role12 = str_match(V1, "((?i)S9)")[, 2]) %>%
+  mutate(role13 = str_match(V1, "((?i)Provider)")[, 2]) %>%
+  mutate(role = coalesce(role1, role2, role3, role4, role5, role6, role7, role8, role9, 
+                         role10, role11, role12, role13))
+
+#removing all extra role columns after I made the final role column using coalesce
+all_transcripts_final <-select(all_transcripts_final, -c("role1", "role2", "role3", "role4", 
+                                                         "role5", "role6", "role7", "role8", 
+                                                         "role9", "role10", "role11", "role12", "role13"))
 
 
+#remove all content that have brackets [....]
+all_transcripts_final$V2 <- gsub("\\[(.*?)\\]", "", all_transcripts_final$V2)
+
+#combine all text by patient_id and role
+all_transcripts_final_combined_roles <- all_transcripts_final %>% 
+  group_by(patient_id, role, date) %>%
+  summarise(V2 = paste(V2, collapse = "")) %>% 
+  ungroup()
