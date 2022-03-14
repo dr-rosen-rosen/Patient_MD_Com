ce7b10c0b99ed7f69138d70179cf382534cb9483
Mike Rosen <mrosena@gmail.com>
2022-03-13 21:06:49 -0400

    Adding turn by turn ananlysis script

---
+++ ECHO/4_turnByturrnLSM.R
@@ -0,0 +1,55 @@
+#########################################
+#### tur by turn LSM
+#########################################
+library(tidyverse)
+# read in turn by turn LIWC file
+df_tbyt <- read.csv('/Users/mrosen44/Johns Hopkins/Salar Khaleghzadegan - Patient _Provider_Communication_Projects/ECHO1_Study/ECHO_Transcripts_Complete_TbyT_LIWC_031222.csv')
+
+
+rLSM_df <- df_tbyt %>%
+  dplyr::select(File, Speaker, Sequence, auxverb, article, adverb, ipron, 
+                prep, negate, conj, quant, ppron) %>%
+  group_by(File) %>%
+  #rowwise() %>%
+  # This puts the turn before the current one on the same row with a .lag suffix
+  #dplyr::mutate(across(.cols=everything(), .funs = ~ dplyr::lead(.x,order_by=File,n = 1, default = NA), .names = '{.col}_lead')) %>%
+  mutate(
+    auxverb.lag = lag(auxverb),
+    article.lag = lag(article),
+    adverb.lag = lag(adverb),
+    ipron.lag = lag(ipron),
+    prep.lag = lag(prep),
+    negate.lag = lag(negate),
+    conj.lag = lag(conj),
+    quant.lag = lag(quant),
+    ppron.lag = lag(ppron)) %>%
+  ungroup() %>%
+  # This makes sure that only liwc categories prersent in the first statement are used for rlsm
+  mutate(across(c(auxverb.lag, article.lag, adverb.lag, ipron.lag, 
+                  prep.lag, negate.lag, conj.lag, quant.lag, ppron.lag), 
+                ~ if_else(. > 0,.,as.numeric(NA)))) %>%
+  rowwise() %>%
+  mutate(
+    auxverb.rLSM = 1 - (abs(auxverb - auxverb.lag) / (auxverb + auxverb.lag + .0001)),
+    article.rLSM = 1 - (abs(article - article.lag) / (article + article.lag + .0001)),
+    adverb.rLSM = 1 - (abs(adverb - adverb.lag) / (adverb + adverb.lag + .0001)),
+    ipron.rLSM = 1 - (abs(ipron - ipron.lag) / (ipron + ipron.lag + .0001)),
+    prep.rLSM = 1 - (abs(prep - prep.lag) / (prep + prep.lag + .0001)),
+    negate.rLSM = 1 - (abs(negate - negate.lag) / (negate + negate.lag + .0001)),
+    conj.rLSM = 1 - (abs(conj - conj.lag) / (conj + conj.lag + .0001)),
+    quant.rLSM = 1 - (abs(quant - quant.lag) / (quant + quant.lag + .0001)),
+    ppron.rLSM = 1 - (abs(ppron - ppron.lag) / (ppron + ppron.lag + .0001))
+  ) %>%
+  ungroup() %>%
+  # creates an averrage rLSM across separrate featurers
+  mutate(rLSM = rowMeans(select(.,contains('.rLSM')),na.rm = TRUE)) %>%
+  mutate(rLSM = ifelse(is.na(rLSM), 0, rLSM)) %>% # replaces NAs with 0
+  group_by(File,Speaker) %>%
+  summarize(rLSM = mean(rLSM)) %>%
+  ungroup() %>%
+  pivot_wider(id_cols = File, names_from = Speaker, values_from = rLSM, names_prefix = 'rLSM.')
+
+readr::write_csv(rLSM_df , 'rLSM.csv')
+
+
+
