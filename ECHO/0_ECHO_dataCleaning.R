9c7ad363302e1234e3b2bd1388a82d968d14f74c
salarkhaleghzadegan <skhaleg1@gmail.com>
2022-03-19 12:44:31 -0400

    Adding updates to the data cleaning script

--- ECHO/0_ECHO_dataCleaning.R
+++ ECHO/0_ECHO_dataCleaning.R
@@ -4,12 +4,18 @@
 library(tidyverse)
 library(here)
 library(config)
+library(gdata)
 
 Sys.setenv(R_CONFIG_ACTIVE = "salar") # 'default')#
 config <- config::get()
 
-#open file
+#open files
 ECHO_LSM_MLM <- read_csv(here(config$ECHO_LSM_MLM_path, config$ECHO_LSM_MLM_name))
+ECHO_LSM_TbyT <- read_csv(here(config$ECHO_LSM_TbyT_path, config$ECHO_LSM_TbyT_name))
+
+#merge TbyT scores with main dataframe
+ECHO_LSM_MLM <-left_join(ECHO_LSM_MLM, ECHO_LSM_TbyT, by = "File")
+
 
 #making a race concordance variable "raceconc" between patient and provider race
 ECHO_LSM_MLM <- ECHO_LSM_MLM %>%
@@ -25,7 +31,7 @@ ECHO_LSM_MLM <- ECHO_LSM_MLM %>%
   mutate(function_sum_doctors = sum(auxverb_D, article_D, adverb_D, ipron_D, 
                                     prep_D, negate_D, conj_D, quant_D, ppron_D))
 
-#getting sd of function_sum_doctors for all patient visits for individual doctors
+#getting sd of function_sum_doctors for all patient visits for individual doctors (style of doctors)
 ECHO_LSM_MLM <- ECHO_LSM_MLM %>%
   group_by(provider_id) %>%
   mutate(provider_style_sd = sd(function_sum_doctors))
@@ -67,38 +73,44 @@ ECHO_LSM_MLM$negemo_P_scaled <- scale(ECHO_LSM_MLM$negemo_P, center = TRUE, scal
 ECHO_LSM_MLM$negemo_D_scaled <- scale(ECHO_LSM_MLM$negemo_D, center = TRUE, scale = TRUE)
 
 
+# drop physicians with < n cases
+ECHO_LSM_MLM <- ECHO_LSM_MLM %>%
+  group_by(provider_id) %>%
+  filter(n() > 7) %>%
+  ungroup() %>%
+  gdata::drop.levels(.)
+
+#count_provider_id <- ECHO_LSM_MLM %>% count(provider_id)
+
+ECHO_LSM_MLM <- ECHO_LSM_MLM %>%
+  mutate(provider_id = factor(provider_id))
+
+
 
 
 
 ############################################################################################################
-#LSM(outcome) will be [lower with racial/ethnic minority patients]
-H1.1.1_df <- ECHO_LSM_MLM %>%
-  dplyr::select(provider_id, LSM_function_mean, racecat2) %>%
-  dplyr::filter(racecat2 !=4) %>%
-  #added levels to the factor because the order without it was 2, 1, 3
-  mutate(racecat2 = factor(racecat2, levels = c("1","2","3"))) %>%
-  #mutate(across(!provider_id & !racecat2, ~scale(.,center = TRUE, scale = TRUE))) %>%
-  tidyr::drop_na()
-
-
-
-############################################################################################################
-#Hypothesis: LSM(outcome) is lower with racial/ethnic minority patients]
-#Added 
-H1.1.2_df <- ECHO_LSM_MLM %>%
-  dplyr::select(provider_id, LSM_function_mean, racecat2, 
-                cdspeak, cdreason, cdstyle, cdrace,
+#Hypothesis: LSM(outcome) will be [lower with racial/ethnic minority patients]
+H1.1_df <- ECHO_LSM_MLM %>%
+  dplyr::select(provider_id, LSM_function_mean, racecat2, rLSM.P, rLSM.D, racecat2, 
+                cdspeak, cdreason, cdstyle, cdrace, cdsubscale,
                 age, gender, working, marital, hsdegree) %>%
   dplyr::filter(racecat2 !=4) %>%
   #added levels to the factor because the order without it was 2, 1, 3
   mutate(racecat2 = factor(racecat2, levels = c("1","2","3"))) %>%
+  mutate(gender = factor(gender)) %>%
+  mutate(working = factor(working)) %>%
+  mutate(marital = factor(marital)) %>%
+  mutate(hsdegree = factor(hsdegree)) %>%
   #mutate(across(!provider_id & !racecat2, ~scale(.,center = TRUE, scale = TRUE))) %>%
   tidyr::drop_na()
+
+
 
 ############################################################################################################
 #Hypothesis: LSM (outcome) is higher within dyads characterized by race concordance
 H1.2_df <- ECHO_LSM_MLM %>%
-  dplyr::select(provider_id, LSM_function_mean, raceconc) %>%
+  dplyr::select(provider_id, LSM_function_mean, raceconc, rLSM.P, rLSM.D) %>%
   mutate(raceconc = factor(raceconc)) %>%
   tidyr::drop_na()
 
@@ -106,7 +118,7 @@ H1.2_df <- ECHO_LSM_MLM %>%
 #Hypothesis: LSM (outcome) is higher for those who rate each other as culturally similar
 #need to add the dissimilarity distance variable
 H1.3_df <- ECHO_LSM_MLM %>%
-  dplyr::select(provider_id, LSM_function_mean, racecat2, 
+  dplyr::select(provider_id, LSM_function_mean, racecat2, rLSM.P, rLSM.D,
                 cultdiss, cultdissmd,
                 cdspeak,cdreason,cdstyle,cdvalue,cdspirit,cdethnic,cdtype,cdrace,cdculture,cdskin,
                 cultdissmd1, cultdissmd2, cultdissmd3, cultdissmd4, cultdissmd5, cultdissmd6, cultdissmd7, cultdissmd8, cultdissmd9, cultdissmd10) %>%
@@ -143,7 +155,7 @@ H1.3_df <- H1.3_df %>%
 ############################################################################################################
 #Using LSM_function_mean to predict communication quality with provider (provcomm)
 H3a.1_df <- ECHO_LSM_MLM %>%
-  dplyr::select(LSM_function_mean, provider_id, provider_style_sd, 
+  dplyr::select(LSM_function_mean, rLSM.P, rLSM.D, provider_id, provider_style_sd, 
                 provcomm, provcommhigh, pcwords, pcfast, pctime,	
                 pclisten, pcignore,	pcinfo,	pchealthprob,	
                 pcanytest, pcwhytest,	pchowtest, pcexamine,	
@@ -162,7 +174,7 @@ H3a.1_df <- ECHO_LSM_MLM %>%
 ############################################################################################################
 #Using LSM_function_mean to predict overall communication quality (overcomm)
 H3a.2_df <- ECHO_LSM_MLM %>%
-  dplyr::select( provider_id, LSM_function_mean, provider_style_sd, overcomm, overcommhigh, ocexplain,	
+  dplyr::select( provider_id, LSM_function_mean, rLSM.P, rLSM.D, provider_style_sd, overcomm, overcommhigh, ocexplain,	
                  ocgive, octell, occare, ocunderstand, WC_D_scaled, WC_P_scaled, WPS_D_scaled, WPS_P_scaled, Sixltr_D_scaled, Sixltr_P_scaled, 
                  affiliation_D_scaled,affiliation_P_scaled, i_D_scaled, i_P_scaled, Clout_D_scaled, 
                  Clout_P_scaled, differ_D_scaled, differ_P_scaled, Clout_D_scaled, Clout_P_scaled, 
@@ -174,7 +186,7 @@ H3a.2_df <- ECHO_LSM_MLM %>%
 ############################################################################################################
 #Using LSM_function_mean to predict provider interpersonal style (ipstyle)
 H3a.3_df <- ECHO_LSM_MLM %>%
-  dplyr::select(LSM_function_mean, provider_id, provider_style_sd, ipstyle, ipstylehigh, 
+  dplyr::select(LSM_function_mean, rLSM.P, rLSM.D, provider_id, provider_style_sd, ipstyle, ipstylehigh, 
                 ipfriend,	ipwelcome, iprude, ipcare,	
                 ipname,	iptalkfront, ippriv, ipinferior,	
                 ipnegattitude, ipdiscrimrace, ipdiscrimeduc,	
@@ -190,7 +202,7 @@ H3a.3_df <- ECHO_LSM_MLM %>%
 ############################################################################################################
 #Using LSM_function_mean to predict interpersonal trust (iptrust)
 H3a.4_df <- ECHO_LSM_MLM %>%
-  dplyr::select(LSM_function_mean, provider_id, provider_style_sd, iptrust, iptrusttert, 
+  dplyr::select(LSM_function_mean, rLSM.P, rLSM.D, provider_id, provider_style_sd, iptrust, iptrusttert, 
                 iptdoubtcare,	iptconsiderate,	iptadvice,	
                 ipttrue, iptdistrust, iptjudge,	iptnotdo,	
                 iptaboveall, iptwellqual, iptmistake, iptinfopriv,
@@ -202,10 +214,11 @@ H3a.4_df <- ECHO_LSM_MLM %>%
   mutate(racecat2 = factor(racecat2, levels = c("1","2","3"))) %>%
   tidyr::drop_na()
 
+H3a.4_df$iptrust_scaled <- scale(H3a.4_df$iptrust, center = TRUE, scale = TRUE)
 ############################################################################################################
 #Using LSM_function_mean to predict if patient reports that provider knows them as a person (provknowcat)
 H3a.5_df <- ECHO_LSM_MLM %>%
-  dplyr::select(LSM_function_mean, provider_id, provider_style_sd, provknowcat, 
+  dplyr::select(LSM_function_mean, rLSM.P, rLSM.D, provider_id, provider_style_sd, provknowcat, 
                 WC_D_scaled, WC_P_scaled, WPS_D_scaled, WPS_P_scaled, Sixltr_D_scaled, Sixltr_P_scaled, 
                 affiliation_D_scaled,affiliation_P_scaled, i_D_scaled, i_P_scaled, Clout_D_scaled, 
                 Clout_P_scaled, differ_D_scaled, differ_P_scaled, Clout_D_scaled, Clout_P_scaled, 
@@ -218,7 +231,7 @@ H3a.5_df <- ECHO_LSM_MLM %>%
 ############################################################################################################
 #Using LSM_function_mean to predict overall patient satisfaction (overallsat)
 H3a.6_df <- ECHO_LSM_MLM %>%
-  dplyr::select(LSM_function_mean, provider_id, provider_style_sd, overallsat,
+  dplyr::select(LSM_function_mean, rLSM.P, rLSM.D, provider_id, provider_style_sd, overallsat,
                 WC_D, WC_P, WPS_D, WPS_P, Sixltr_D, Sixltr_P, 
                 affiliation_D,affiliation_P, i_D, i_P, Clout_D, 
                 Clout_P, differ_D, differ_P, Clout_D, Clout_P, 
@@ -231,7 +244,7 @@ H3a.6_df <- ECHO_LSM_MLM %>%
 ############################################################################################################
 #Using LSM_function_mean to predict viral suppression
 H3b.1_df <- ECHO_LSM_MLM %>%
-  dplyr::select(LSM_function_mean, provider_id, vlsup75,
+  dplyr::select(LSM_function_mean, rLSM.P, rLSM.D, provider_id, vlsup75,
                 WC_D_scaled, WC_P_scaled, WPS_D_scaled, WPS_P_scaled, Sixltr_D_scaled, Sixltr_P_scaled, 
                 affiliation_D_scaled,affiliation_P_scaled, i_D_scaled, i_P_scaled, Clout_D_scaled, 
                 Clout_P_scaled, differ_D_scaled, differ_P_scaled, Clout_D_scaled, Clout_P_scaled, 
@@ -245,7 +258,7 @@ H3b.1_df <- ECHO_LSM_MLM %>%
 #Using LSM_function_mean to predict adherence measures
 #adhard, adeasy, adunable, adfollow,adpast4, adpast30, pctarv, adrate, missany(binomial)
 H4_df <- ECHO_LSM_MLM %>%
-  dplyr::select(LSM_function_mean, provider_id, provider_style_sd, racecat2, adhard, adeasy, adunable, adfollow,
+  dplyr::select(LSM_function_mean, rLSM.P, rLSM.D, provider_id, provider_style_sd, racecat2, adhard, adeasy, adunable, adfollow,
                 adpast4, adpast30, pctarv, adrate, missany, WC_D_scaled, WC_P_scaled, WPS_D_scaled, WPS_P_scaled, Sixltr_D_scaled, Sixltr_P_scaled, 
                 affiliation_D_scaled,affiliation_P_scaled, i_D_scaled, i_P_scaled, Clout_D_scaled, 
                 Clout_P_scaled, differ_D_scaled, differ_P_scaled, Clout_D_scaled, Clout_P_scaled, 
