c9b6c1fa07bd8d3d111d6e9bd4df10f5807cbe5b
Mike Rosen <mrosena@gmail.com>
2022-05-23 09:28:10 -0400

    Tweaks to elastic net

--- ECHO/5_elasticNet.R
+++ ECHO/5_elasticNet.R
@@ -3,32 +3,52 @@
 library(tidyverse)
 library(glmnet)
 library(glmnetUtils)
-library(rCom)
+
 
 ###################
 ###################
 
 # Define the IV's and DV's
 
-iv <- 'ipstylehigh'
-iv <- 'ipstyle'
-iv <- 'provassess'
+
+# iv <- 'provassess'
 iv <- 'iptrust'
 iv <- 'provcomm'
+iv <- 'overcomm'
 
+
+###################
+###################
+# IP Style
+###################
+###################
+iv <- 'ipstylehigh'
+iv <- 'ipstyle'
+
+# Matching
 dvs <- ECHO_Matching_variables
 allVars <- c(iv,dvs)
 # get data set of complete cases for iv and all dvs
 df_NoNA <- ECHO_All_Matching[complete.cases(ECHO_All_Matching[,allVars]),allVars]
+# df_NoNA$ipstylehigh <- as.factor(df_NoNA$ipstylehigh)
 # This uses everything accept the iv as a predictor
-cv.glmmod <- cv.glmnet(iptrust ~ ., alpha = .4, data = df_NoNA, family = 'gaussian') 
-plot(cv.glmmod)
-best_lambda <- cv.glmmod$lambda.min # lambda.min or lambda.1se
-coef(cv.glmmod, s = "lambda.min")
+ipstyle.cv.glmmod <- glmnetUtils::cv.glmnet(ipstyle ~ ., alpha = .2, data = df_NoNA, family = 'gaussian')
+plot(ipstyle.cv.glmmod)
+# ipstyle.best_lambda <- ipstyle.cv.glmmod$lambda.min # lambda.min or lambda.1se
+coef(ipstyle.cv.glmmod, s = "lambda.min")
 
 # the above tries to optimize lambad with a given alpha; 
 # this extention tries to optimize both alpha and lambda
 # still working on intrepreting this
-cva <- cva.glmnet(iptrust ~., data = df_NoNA, family = 'gaussian')
+cva <- glmnetUtils::cva.glmnet(iptrust ~., data = df_NoNA, family = 'gaussian')
 plot(cva)
 predict(cva, df_NoNA,alpha = .001)
+
+# Speach features
+
+###################
+###################
+# IP Style
+###################
+###################
+
